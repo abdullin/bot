62598a82d19a1b47d3d015685ae8a72837904950
Rinat Abdullin <rinat.abdullin@gmail.com>
2019-01-17 12:09:24 +0500

    Bot prototype

---
+++ app.py
@@ -0,0 +1,20 @@
+from telegram.ext import Updater, CommandHandler
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+
+updater = Updater('268340955:AAH2VAP-MSirp68M64WvfqJTsqSj8wxzzic')
+dispatcher = updater.dispatcher
+
+
+def echo(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+
+
+from telegram.ext import MessageHandler, Filters
+echo_handler = MessageHandler(Filters.text, echo)
+dispatcher.add_handler(echo_handler)
+
+updater.start_polling()
+updater.idle()
