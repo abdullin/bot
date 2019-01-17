0cf69b131a041ddae29528e52a2fab52dc5d1355
Rinat Abdullin <rinat.abdullin@gmail.com>
2019-01-17 12:22:17 +0500

    Print message date

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from telegram import Update, Message
 from telegram.ext import Updater, CommandHandler
+
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -8,9 +10,12 @@ updater = Updater('268340955:AAH2VAP-MSirp68M64WvfqJTsqSj8wxzzic')
 dispatcher = updater.dispatcher
 
 
-def echo(bot, update):
+def echo(bot, update: Update):
     print(type(update))
-    
+    m : Message = update.message
+    print(update.message.date)
+
+
     bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 
 
