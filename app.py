a03bcc9fc808e602242ec589a7e4933f4b7db282
Rinat Abdullin <rinat.abdullin@gmail.com>
2019-01-17 12:42:10 +0500

    Record today records

--- app.py
+++ app.py
@@ -19,14 +19,32 @@ updater = Updater('268340955:AAH2VAP-MSirp68M64WvfqJTsqSj8wxzzic')
 dispatcher = updater.dispatcher
 
 
+import json
+
+def append_todo(local, text):
+    file = local.strftime('%Y-%m-%d') + ".json"
+    with open(file, mode='a+', encoding='utf-8') as js:
+        json.dump({'text': text, 'time': local}, js)
+
+
+
+
 def echo(bot, update: Update):
-    print(type(update))
+
     m : Message = update.message
-    print(utc_to_local(update.message.date))
 
+    local = utc_to_local(update.message.date)
 
-    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+    text = update.message.text
 
+    cmd = text.split(' ', 1)[0].lower()
+
+    if cmd == 't':
+        append_todo(local, text)
+        bot.send_message(chat_id=update.message.chat_id, text='+today')
+        return
+
+    bot.send_message(chat_id=update.message.chat_id, text='wat?')
 
 from telegram.ext import MessageHandler, Filters
 echo_handler = MessageHandler(Filters.text, echo)
